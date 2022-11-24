dde6d2cdc501c28df262ab5024ac55090634b24b
Lobel <lobel.spehar.os@gmail.com>
2022-11-24 22:50:03 +0100

    Practice - sql select

---
+++ SQL/bazeZaVjezbanje/naredbe.sql
@@ -0,0 +1,54 @@
+# mysql -uroot --default_character_set=utf8 < 
+
+show tables;
+
+select * from autor;
+select * from izdavac;
+select * from katalog;
+select * from mjesto;
+
+select ime from autor;
+
+select ime,prezime from autor;
+
+select count(ime) from autor;
+
+select count(distinct ime) from autor;
+
+select  ime,prezime from autor where prezime like ('%da%') order by ime limit 10;
+
+select * from autor where ime like ('%da%') and prezime like ('%ne%') order by prezime limit 10;
+
+select * from autor where ime not like ('%a%') and ime not like ('%u%');
+
+select * from katalog where naslov not like ('%1%') order by naslov desc limit 10;
+
+select min(sifra) as minimum from katalog;
+
+select max(ime) as ime from autor;
+
+select count(*) from autor where ime like '%da%';
+
+select avg(postanskiBroj) from mjesto;
+
+select sum(sifra) from autor where ime not like '%a';
+
+select ime from autor where ime like 'a%a' order by ime limit 10;
+
+select ime from autor where ime like 'a%a' order by ime limit 10;
+
+select count(ime) from autor where not ime like '%a%' and prezime like '%a%';
+
+select * from katalog order by izdavac,naslov desc;
+
+select naslov from katalog where izdavac between 500 and 515 order by naslov;
+
+delete from katalog;
+
+select ime from autor where ime like '_a_a';
+
+select prezime from autor where prezime like '_a_b%'
+
+select count(ime) from autor where ime in('kata','fata');
+
+select * from katalog group by autor order by count(autor) desc;
